642003bdf4dfc0371be9793acb4a33f2a052a07b
walax31 <walax31009@gmail.com>
2022-04-14 20:23:21 +0700

    added eslint

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    env: {
-        "jest": true
-    },
-    extends: ["standard", "plugin:prettier/recommended"],
-    "prettier/prettier": ["error", { "singleQuote": true }]
-}
